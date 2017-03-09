717afb3f7056636eba9c6285541746933f52f612
Muhammed Mohsen <muhammedmohsen@kernel.blue>
2017-03-09 07:06:05 +0200

    Update Service

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
+var moment = require('moment')
 var express = require('express')
+var path = require('path')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+
+app.get('/:timestamp', function (req, res) {
+  var timestamp
+  if (/^\d{8,}$/.test(req.params.timestamp)){
+    timestamp = moment(req.params.timestamp, "X");
+  }
+  else {
+    timestamp = moment(req.params.timestamp, "MMMM DD, YYYY");
+  }
+
+  if(timestamp.isValid()) {
+    res.json({
+      unix: timestamp.format("X"),
+      natural: timestamp.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+})
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname,'index.html'),function(err){
+    if (err){
+    console.log(err)
+    res.status(err.status).end()
+    }
+    else {
+      console.log("index.html loaded")
+    }
+  })
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Timestamp API is listening on port 3000!')
 })
